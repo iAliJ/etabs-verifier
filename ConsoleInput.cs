79617500263f58a291a6de462fd7910407cd38c9
Ali <47180374+iAliJ@users.noreply.github.com>
2020-10-08 17:16:56 +0300

    Add column property modifiers functionality

---
+++ ConsoleInput.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EtabsAPI.ConsoleServices
+{
+    // Responsibility of this class is to get and promt for input from the user.
+    public class ConsoleInput
+    {
+        private static string userInput;
+        public static void GetInput()
+        {
+            Console.WriteLine("Input a valid command from the list below");
+            ShowCommands();
+            userInput = Console.ReadLine();
+            if(!InputServices.isValidInput(userInput))
+            {
+                ConsoleInput.GetInput();
+            };
+            InputServices.CallCommand(userInput);
+        }
+
+        // Show available commands to the user
+        private static void ShowCommands()
+        {
+            string commands = "";
+            commands += $"{CommandsList.Beam.ToString()}: Modify beam properties\n";
+            commands += $"{CommandsList.Column.ToString()}: Modify column properties\n";
+            commands += $"{CommandsList.Exit.ToString()}: Exit the tool\n";
+            
+            Console.WriteLine(commands);
+        }
+
+        public static void ShowBeamCommands()
+        {
+            string commands = $"{BeamCommandsList.modifiers.ToString()}: Modify beam properties modifiers\n";
+            // For now this tool will only set beam modifiers
+            //string commands = $"{BeamCommandsList.sections.ToString()}: Modify beam section properties\n";
+            Console.WriteLine(commands);
+        }
+
+        public static void GetBeamCommands()
+        {
+            // Get beam valid command from the user then call the input services to process it
+            Console.WriteLine("Input a valid command from the list above");
+            userInput = Console.ReadLine();
+            if(!InputServices.isValidInput(userInput))
+            {
+                ConsoleInput.GetBeamCommands();
+            }
+            InputServices.CallBeamCommand(userInput);
+        }
+
+        public static void GetBeamModifier()
+        {
+            string message = "input beam modifier values array in the following order:\n";
+            message += "A, V1, V2, T, I2, I3, Mass, Weight";
+            Console.WriteLine(message);            
+            string userInput = Console.ReadLine();            
+            InputServices.ProcessBeamModifier(userInput);
+        }
+
+        public static void ShowColumnCommands()
+        {
+            string commands = $"{ColumnCommandsList.modifiers.ToString()}: Modify All columns property modifiers\n";
+            Console.WriteLine(commands);
+        }
+
+        public static void GetColumnCommands()
+        {
+            Console.WriteLine("Input a valid command from the list above");
+            userInput = Console.ReadLine();
+            if(!InputServices.isValidInput(userInput))
+            {
+                ConsoleInput.GetColumnCommands();
+            }
+            InputServices.CallColumnCommand(userInput);
+        }
+
+        public static void GetColumnModifier()
+        {
+            string message = "input column modifier values array in the following order:\n";
+            message += "A, V1, V2, T, I2, I3, Mass, Weight";
+            Console.WriteLine(message);            
+            string userInput = Console.ReadLine();            
+            InputServices.ProcessBeamModifier(userInput);
+        }
+    }
+}
---
+++ InputServices.cs
@@ -0,0 +1,107 @@
+using System;
+
+namespace EtabsAPI.ConsoleServices
+{    
+    // resposiblity of this class is to process the user input
+    public class InputServices
+    {
+        // Classify user input and call the related methods
+        // This method should be called only after isValidInput() has been called.
+        public static void CallCommand(string userInput)
+        {
+            CommandsList userCommand = (CommandsList) Enum.Parse(typeof(CommandsList), userInput, true);
+            switch(userCommand)
+            {
+                case CommandsList.Beam:
+                    Console.WriteLine("Calling beam commands ...");
+                    ConsoleInput.ShowBeamCommands();
+                    ConsoleInput.GetBeamCommands();
+                    break;
+                case CommandsList.Column:
+                    // perform action on columns
+                    Console.WriteLine("Calling column commands ...");
+                    ConsoleInput.ShowColumnCommands();
+                    ConsoleInput.GetColumnCommands();
+                    break;
+                case CommandsList.Exit:
+                    // Exit the tool
+                    break;
+            }
+        }
+
+        public static void CallBeamCommand(string userInput)
+        {
+            BeamCommandsList userCommand = (BeamCommandsList) Enum.Parse(typeof(BeamCommandsList), userInput, true);
+            switch(userCommand)
+            {
+                case BeamCommandsList.modifiers:
+                // Call beam modifiers commands
+                ConsoleInput.GetBeamModifier();
+                break;
+
+                case BeamCommandsList.sections:
+                // Cal beam sections commands
+                break;
+            }
+        }
+
+        public static void CallColumnCommand(string userInput)
+        {
+            ColumnCommandsList userCommand = (ColumnCommandsList) Enum.Parse(typeof(ColumnCommandsList), userInput, true);
+            switch(userCommand)
+            {
+                case ColumnCommandsList.modifiers:
+                // Call column modifiers commands
+                ConsoleInput.GetColumnModifier();
+                break;
+
+                case ColumnCommandsList.sections:
+                // Cal column sections commands
+                break;
+            }
+        }
+        // This method will validate if user input is matching with commands list.
+        public static bool isValidInput(string userInput)
+        {
+            bool isValid = false;
+            // validate initial commends
+            foreach(var command in Enum.GetNames(typeof(CommandsList)))
+            {   
+                if(command.ToUpper() == userInput.ToUpper())
+                {
+                    Console.WriteLine("Valid input");
+                    isValid = true;
+                    break;
+                }
+            }
+
+            // validate beam commands
+            foreach(var command in Enum.GetNames(typeof(BeamCommandsList)))
+            {   
+                if(command.ToUpper() == userInput.ToUpper())
+                {
+                    Console.WriteLine("Valid beam input");
+                    isValid = true;
+                    break;
+                }
+            }
+            return isValid;
+        }
+
+        public static void ProcessBeamModifier(string userInput)
+        {
+            // Convert input to array then call InputParser method
+            string[] Smodifiers = userInput.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+            double[] modifiers = Array.ConvertAll(Smodifiers, new Converter<string, double>(Double.Parse));
+            InputParser.ChangeColumnModifier(modifiers);
+        }
+
+        public static void ProcessColumnModifier(string userInput)
+        {
+            // Convert input to array then call InputParser method
+            string[] Smodifiers = userInput.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+            double[] modifiers = Array.ConvertAll(Smodifiers, new Converter<string, double>(Double.Parse));
+            InputParser.ChangeBeamModifier(modifiers);
+        }
+    }
+}
